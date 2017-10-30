53fc5117da4ca7a98fd3a4c9ac47f541190b7d29
Talita Lira <lira.talita@hotmail.com>
2017-10-30 03:55:10 -0200

    adding app

---
+++ aFazenda/src/app/interfaces/people.ts
@@ -0,0 +1,11 @@
+export class People{
+    __id: string;
+    timestamp: number;
+    name: string;
+    description: string;
+    picture: string;
+    positive: number;
+    negative: number;
+    likes: number;
+    dislikes: number;
+}
